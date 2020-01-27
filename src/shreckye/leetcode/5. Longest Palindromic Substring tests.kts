f5fbdb91b682725c855b6d8729d08615e3a7d5ba
Shreck Ye <ShreckYe@gmail.com>
2020-01-27 19:39:36 +0800

    Add question numbers

--- src/shreckye/leetcode/5. Longest Palindromic Substring tests.kts
+++ src/shreckye/leetcode/5. Longest Palindromic Substring tests.kts
@@ -5,8 +5,8 @@ fun CharRange.randomString(length: Int) =
     String(CharArray(length) { random() })
 
 val LOWERCASE_ALPHABETS = 'a'..'z'
-val on2Solution = Longest_Palindromic_Substring_O_n_2_.Solution()
-val manacherSolution = Longest_Palindromic_Substring_Manacher_s_algorithm.Solution()
+val on2Solution = _5__Longest_Palindromic_Substring_O_n_2_.Solution()
+val manacherSolution = _5__Longest_Palindromic_Substring_Manacher_s_algorithm.Solution()
 repeat(1000) {
     println("Random test $it")
     val s = LOWERCASE_ALPHABETS.randomString(100)
