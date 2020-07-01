d546c61a0cb266e192db454d4a9f0626515e5a9b
Shreck Ye <ShreckYe@gmail.com>
2020-07-01 21:52:56 +0800

    Update code to query `HashMap` only once for each number in Question 1

--- src/shreckye/leetcode/1. Two Sum hash table.kts
+++ src/shreckye/leetcode/1. Two Sum hash table.kts
@@ -6,7 +6,8 @@ class Solution {
         val numMap = HashMap<Int, Int>(nums.size)
         for ((i, num) in nums.withIndex()) {
             val complement = target - num
-            if (numMap.containsKey(complement)) return intArrayOf(numMap[complement]!!, i)
+            val complementIndex = numMap[complement]
+            if (complementIndex !== null) return intArrayOf(complementIndex, i)
             numMap[num] = i
         }
         throw IllegalArgumentException()
