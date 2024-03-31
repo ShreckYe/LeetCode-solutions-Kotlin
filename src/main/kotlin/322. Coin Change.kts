a75f32e6093d4262cdc5286f8926de888a245f09
Shreck Ye <ShreckYe@gmail.com>
2024-03-31 18:27:59 +0800

    Add a comment to the solution to 322. Coin Change

--- src/main/kotlin/322. Coin Change.kts
+++ src/main/kotlin/322. Coin Change.kts
@@ -3,7 +3,7 @@ class Solution {
         val minNums = IntArray(amount + 1) { -1 }
         minNums[0] = 0
         for (i in 1..amount)
-            minNums[i] = coins.asSequence()
+            minNums[i] = coins.asSequence() // using `.asList()` is even slower
                 .mapNotNull { coin -> minNums.getOrNull(i - coin)?.let { if (it != -1) it + 1 else null } }
                 .minOrNull() ?: -1
         return minNums[amount]
