f9622c0676ecde2457a0c19dba6d796576c22b24
Shreck Ye <ShreckYe@gmail.com>
2024-03-31 18:26:02 +0800

    Add a solution to 322. Coin Change

---
+++ src/main/kotlin/322. Coin Change.kts
@@ -0,0 +1,11 @@
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        val minNums = IntArray(amount + 1) { -1 }
+        minNums[0] = 0
+        for (i in 1..amount)
+            minNums[i] = coins.asSequence()
+                .mapNotNull { coin -> minNums.getOrNull(i - coin)?.let { if (it != -1) it + 1 else null } }
+                .minOrNull() ?: -1
+        return minNums[amount]
+    }
+}
